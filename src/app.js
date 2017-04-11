cca789fb298609f4c05ffc87b27f0dda1a68dd23
Marco Bruno <marco.bruno.br@gmail.com>
2017-04-11 03:37:51 -0300

    Created the response to /allEvents

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('Olá!')
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const chat = new Telegram.Telegram('371045027:AAFg6dZiZxJr36qhABQKbgNXsnv7VxkOuiE')
+
+class EventsController extends TelegramBaseController {
+  allEvents(scope) {
+    scope.sendMessage(`QConSP - 24-25-26/04/2017 - qconsp.com\nFrontInSampa - 01/07/2017 - frontinsampa.com.br\nAgileBrazil - 13-14-15/09/2017 - agilebrazil.com/2017`)
+  }
+
+  get routes() {
+    return {
+      'allEvents': 'allEvents'
+    }
+  }
+}
+
+chat.router
+    .when(
+      new TextCommand('/allevents', 'allEvents'), new EventsController()
+    )
